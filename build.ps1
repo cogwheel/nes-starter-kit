44140dd9763b998b3d8f4ddf1080a701d5ab0c05
Matthew Orlando <maorlando@gmail.com>
2023-09-02 13:44:05 -0700

    Fix incorrect build type default

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$env:BUILD_TYPE ?? 'RelWithDebInfo'"
+cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$($env:BUILD_TYPE ?? "RelWithDebInfo")"
 make -C build @args
