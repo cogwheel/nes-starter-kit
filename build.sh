02ab61ae19779cf6e90f2b48a92641b252dba189
Matthew Orlando <maorlando@gmail.com>
2023-09-13 09:04:55 -0700

    Use llvm-mos' clangd for vscode integration

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-RelWithDebInfo}
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-RelWithDebInfo} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 ninja -C build $@
